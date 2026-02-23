ea81485aa4dfc95f43d5411b380c6a59b99ffec3
Neha Chopra <chopraneha211@gmail.com>
2026-02-23 12:03:36 +0530

    Add .gitignore file

---
+++ src/01-page-view-tracker.js
@@ -0,0 +1,22 @@
+// Page view tracker
+// We might have seen the current page we are on when opened a pdf. 
+// Upon scrolling down, the page counter increments and upon scrolling up, the page counter decrements
+// We need to recreate same page viewer which increments and decrements by 1.
+
+// We need to create a function createCounter that returns following functions 
+// 1. increment()
+// 2. decrement()
+// 3. getCount()
+
+// Example
+// const counter = createCounter();
+// counter.increment();
+// counter.increment();
+// console.log(counter.getCount()); // 2
+
+// counter.count = 100; // should NOT affect internal count
+
+function createCounter() {
+    //Your code here
+    
+}
